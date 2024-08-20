eb8b472de0f10cb1c778581b1bb0b707c305eb7e
0x3C <38872626+kjd18@users.noreply.github.com>
2024-08-19 21:06:24 -0700

    Create purgeplus.sh
    guts the build and adds libs for kernel compilation. must be run as root.

---
+++ purgeplus.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# purges debian OS while installing qemu
+apt-get update --fix-missing
+apt-get dist-upgrade
+apt-get install git fakeroot ncurses-dev xz-utils libssl-dev bd flex libelf-dev bison
+apt-get install build-essential gparted git vlc
+git clone https://github.com/LostByteSoft/Debian-10
+cd Debian-10/
+cd Debian_12
+chmod +x remove\ ALL\ deb\ 12.5\ excep\ zzz.sh 
+mv remove\ ALL\ deb\ 12.5\ excep\ zzz.sh rem.sh
+./rem.sh
+apt-get purge libreoffice-common
+sudo apt install qemu-utils qemu-system-* libvirt-daemon-system
+apt autoremove
+apt purge exfalso xfburn parole
+apt-get install vlc
+apt-get autoremove
